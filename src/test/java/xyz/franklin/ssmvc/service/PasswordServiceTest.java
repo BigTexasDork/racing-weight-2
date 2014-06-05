5be6dd28a067dde9768feef3e9cb552c51f19687
Joe Franklin <joe.franklin@gmail.com>
2014-06-04 20:45:54 -0500

    move config file to match others.  add test method.

--- src/test/java/xyz/franklin/ssmvc/service/PasswordServiceTest.java
+++ src/test/java/xyz/franklin/ssmvc/service/PasswordServiceTest.java
@@ -1,14 +1,28 @@
 package xyz.franklin.ssmvc.service;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
 
+import edu.vt.middleware.password.RuleResult;
+
+@RunWith(SpringJUnit4ClassRunner.class)
 public class PasswordServiceTest {
 
+	@Autowired
+	PasswordService passwordService;
+	
+	@Autowired
+	
 	@Test
 	public void testValidatePassword() {
-		fail("Not yet implemented");
-	}
+		
+		  /** Test password. */
+		  final String MIN_VALID_PASS = "p4T3#";
 
+	    RuleResult result = passwordService.validatePassword(MIN_VALID_PASS);
+	    Assert.notNull(result);
+	}
 }
